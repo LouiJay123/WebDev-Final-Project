e786697f3bb6c286d28406bda6adf750c570c1c6
LouiJay123 <154348974+LouiJay123@users.noreply.github.com>
2023-12-19 23:03:24 +0800

    Add files via upload

---
+++ register.php
@@ -0,0 +1,34 @@
+<?php
+
+$is_success = false;
+
+if ($_SERVER["REQUEST_METHOD"] === "POST"){
+    $mysqli = require __DIR__. "/database.php";
+
+    $sql = "INSERT INTO user (name, email, password) VALUES (?,?,?)";
+
+    $stmt = $mysqli->stmt_init();
+
+    if ( ! $stmt->prepare($sql)){
+        die("SQL error: ". $mysqli->error);
+    }
+
+    $stmt->bind_param("sss",
+                        $_POST["name"],
+                        $_POST["email"],
+                        $_POST["password"]);
+
+    if ($stmt->execute()){
+        $is_success = true;
+        header ("location: login.php");
+    }
+    else{
+        die($mssqli->error. " " . $mysqli->errno);
+    }
+}
+
+?>
+
+
+
+
